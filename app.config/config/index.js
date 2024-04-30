978c016322107d74026d357fc77427405653c35c
Hammad Siddiqui <hammadzs@gmail.com>
2024-04-30 14:22:52 -0400

    Revert Labeeb config

--- app.config/config/index.js
+++ app.config/config/index.js
@@ -1,64 +1,22 @@
-import { getTosContent } from "./global/tos";
-import { getSidebarLogo } from "./global/sidebar";
-import { FaFileImport } from "react-icons/fa";
-import { ImportSuggestions } from "./write/actions/ImportSuggestions";
-import { getTags, getTaxonomySets, getTopics } from "./data/taxonomySets";
-import { basePath } from "../../src/utils/constants";
+// This is the master config for the concierge app.
+// To override a particular section, define it here.
+// To see the complete config schema, please refer to
+// /config/default/config/index.js
 
-// The entire Labeeb application can be configured here
-// Note that all assets and locales are copied to the public/app and src/locales directories respectively
-// by the prebuild.js script
 export default {
-    global: {
-        siteTitle: "Labeeb",
-        getLogo: (language) =>
-            (basePath || "") +
-            `/app/assets/labeeb-logo-${language === "ar" ? "ar" : "en"}.png`,
-        getTosContent,
-        getSidebarLogo,
-    },
-    data: {
-        getTaxonomySets,
-        getTopics,
-        getTags,
-    },
-    write: {
-        actions: {
-            import: {
-                Icon: FaFileImport,
-                type: "always-available",
-                title: "Import from UCMS",
-                dialogClassName: "modal-narrow",
-                commitLabel: "Import",
-                SuggestionsComponent: ImportSuggestions,
-                postApply: "clear-headline",
-            },
-        },
-    },
-    chat: {
-        botName: "Labeeb",
-        dataSources: [
-            {
-                key: "wires",
-                name: "Wires",
-                description: "use wires",
-            },
-            {
-                key: "aja",
-                name: "Al Jazeera Arabic articles",
-                description: "use aj arabic articles",
-            },
-            {
-                key: "aje",
-                name: "Al Jazeera English articles",
-                description: "use aj english articles",
-            },
-        ],
-    },
-    code: {
-        botName: "Knuth",
-    },
-    auth: {
-        provider: "entra",
-    },
+    // global: {
+    //   ...
+    // },
+    // data: {
+    //   ...
+    // },
+    // write: {
+    //   ...
+    // },
+    // chat: {
+    //   ...
+    // },
+    // code: {
+    //   ...
+    // }
 };
