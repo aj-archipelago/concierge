eef953c68695d9e4608224ee8f5533bfbc2ae796
Hammad Siddiqui <hammadzs@gmail.com>
2024-07-22 14:26:35 -0400

    ARC-2389: Use hostname from client request to determine GraphQL URL
    To support server-side rendering, we had put this in an environment variable, `SERVER_URL`. However, this means that one deployment for Labeeb can't be used on multiple domains (e.g. labeeb.aljazeera.com and labeeb.aljazeera.net) without encountering a CORS error from the client. The [documentation](https://nextjs.org/docs/app/api-reference/functions/headers#:%7E:text=Using%20it%20in%20a%20layout%20or%20page%20will%20opt%20a%20route%20into%20dynamic%20rendering%20at%20request%20time) says that using this will switch the layout.js to dynamic rendering, because the request.host is not known before request time:
    ```
    headers() is a Dynamic Function whose returned values cannot be known ahead of time. Using it in a layout or page will opt a route into dynamic rendering at request time.
    ```
    If the switch to dynamic rendering causes performance problems, we'll have to find an alternate solution, such as finding a way to enable CORS on the proxying requests.

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -8,18 +8,16 @@ import {
     PlusIcon,
     TrashIcon,
 } from "@heroicons/react/24/outline";
-import { useQueryClient } from "@tanstack/react-query";
-import { HelpCircle, NewspaperIcon } from "lucide-react";
+import { HelpCircle } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FaEdit } from "react-icons/fa";
 import { MdOutlineWorkspaces } from "react-icons/md";
 import {
     useAddChat,
     useDeleteChat,
-    useGetActiveChatId,
     useGetActiveChats,
     useSetActiveChatId,
     useUpdateChat,
