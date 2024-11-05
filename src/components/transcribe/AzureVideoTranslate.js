39766c3b9d0f0ea26aca8a5d1536a0f363972e4a
Jason McCartney <jmac@theroot.org>
2024-11-04 17:22:02 -0700

    Add what should be valid audio languages

--- src/components/transcribe/AzureVideoTranslate.js
+++ src/components/transcribe/AzureVideoTranslate.js
@@ -11,7 +11,7 @@ export default function AzureVideoTranslate({ url }) {
     const [outputVideoUrl, setOutputVideoUrl] = useState(null);
 
     const [sourceLocale, setSourceLocale] = useState("en-US");
-    const [targetLocale, setTargetLocale] = useState("tr-TR");
+    const [targetLocale, setTargetLocale] = useState("ar-QA");
 
     function setFinalDataPre(data) {
         try {
@@ -58,11 +58,80 @@ export default function AzureVideoTranslate({ url }) {
                         className="lb-input"
                         onChange={(e) => setSourceLocale(e.target.value)}
                     >
+                        <option value="ar-AE">ar-AE</option>
+                        <option value="ar-BH">ar-BH</option>
+                        <option value="ar-DZ">ar-DZ</option>
+                        <option value="ar-EG">ar-EG</option>
+                        <option value="ar-IL">ar-IL</option>
+                        <option value="ar-IQ">ar-IQ</option>
+                        <option value="ar-JO">ar-JO</option>
+                        <option value="ar-KW">ar-KW</option>
+                        <option value="ar-LB">ar-LB</option>
+                        <option value="ar-LY">ar-LY</option>
+                        <option value="ar-MA">ar-MA</option>
+                        <option value="ar-OM">ar-OM</option>
+                        <option value="ar-PS">ar-PS</option>
+                        <option value="ar-QA">ar-QA</option>
+                        <option value="ar-SA">ar-SA</option>
+                        <option value="ar-SY">ar-SY</option>
+                        <option value="ar-TN">ar-TN</option>
+                        <option value="ar-YE">ar-YE</option>
+                        <option value="cs-CZ">cs-CZ</option>
+                        <option value="da-DK">da-DK</option>
+                        <option value="de-AT">de-AT</option>
+                        <option value="de-CH">de-CH</option>
+                        <option value="de-DE">de-DE</option>
+                        <option value="en-AU">en-AU</option>
+                        <option value="en-CA">en-CA</option>
+                        <option value="en-GB">en-GB</option>
+                        <option value="en-GH">en-GH</option>
+                        <option value="en-HK">en-HK</option>
+                        <option value="en-IE">en-IE</option>
+                        <option value="en-IN">en-IN</option>
+                        <option value="en-KE">en-KE</option>
+                        <option value="en-NG">en-NG</option>
+                        <option value="en-NZ">en-NZ</option>
+                        <option value="en-PH">en-PH</option>
+                        <option value="en-SG">en-SG</option>
+                        <option value="en-TZ">en-TZ</option>
                         <option value="en-US">en-US</option>
-                        <option value="ar-AR">ar-AR</option>
-                        <option value="fr-FR">fr-FR</option>
+                        <option value="en-ZA">en-ZA</option>
+                        <option value="es-BO">es-BO</option>
+                        <option value="es-CL">es-CL</option>
+                        <option value="es-CO">es-CO</option>
+                        <option value="es-CR">es-CR</option>
                         <option value="es-ES">es-ES</option>
-                        {/* Add more options as needed */}
+                        <option value="es-GQ">es-GQ</option>
+                        <option value="es-GT">es-GT</option>
+                        <option value="es-HN">es-HN</option>
+                        <option value="es-MX">es-MX</option>
+                        <option value="es-NI">es-NI</option>
+                        <option value="es-PA">es-PA</option>
+                        <option value="es-PE">es-PE</option>
+                        <option value="es-PR">es-PR</option>
+                        <option value="es-PY">es-PY</option>
+                        <option value="es-SV">es-SV</option>
+                        <option value="es-UY">es-UY</option>
+                        <option value="fi-FI">fi-FI</option>
+                        <option value="fr-FR">fr-FR</option>
+                        <option value="he-IL">he-IL</option>
+                        <option value="hi-IN">hi-IN</option>
+                        <option value="id-ID">id-ID</option>
+                        <option value="it-IT">it-IT</option>
+                        <option value="ja-JP">ja-JP</option>
+                        <option value="ko-KR">ko-KR</option>
+                        <option value="pa-IN">pa-IN</option>
+                        <option value="pl-PL">pl-PL</option>
+                        <option value="pt-BR">pt-BR</option>
+                        <option value="pt-PT">pt-PT</option>
+                        <option value="ru-RU">ru-RU</option>
+                        <option value="sv-SE">sv-SE</option>
+                        <option value="th-TH">th-TH</option>
+                        <option value="tr-TR">tr-TR</option>
+                        <option value="ur-IN">ur-IN</option>
+                        <option value="zh-CN">zh-CN</option>
+                        <option value="zh-HK">zh-HK</option>
+                        <option value="zh-TW">zh-TW</option>
                     </select>
                 </div>
 
@@ -74,12 +143,80 @@ export default function AzureVideoTranslate({ url }) {
                         value={targetLocale}
                         onChange={(e) => setTargetLocale(e.target.value)}
                     >
-                        <option value="tr-TR">tr-TR</option>
-                        <option value="en-US">en-US</option>
-                        <option value="ar-AR">ar-AR</option>
+                        <option value="ar-AE">ar-AE</option>
+                        <option value="ar-BH">ar-BH</option>
+                        <option value="ar-DZ">ar-DZ</option>
+                        <option value="ar-EG">ar-EG</option>
+                        <option value="ar-IL">ar-IL</option>
+                        <option value="ar-IQ">ar-IQ</option>
+                        <option value="ar-JO">ar-JO</option>
+                        <option value="ar-KW">ar-KW</option>
+                        <option value="ar-LB">ar-LB</option>
+                        <option value="ar-LY">ar-LY</option>
+                        <option value="ar-MA">ar-MA</option>
+                        <option value="ar-OM">ar-OM</option>
+                        <option value="ar-PS">ar-PS</option>
+                        <option value="ar-QA">ar-QA</option>
+                        <option value="ar-SA">ar-SA</option>
+                        <option value="ar-SY">ar-SY</option>
+                        <option value="ar-TN">ar-TN</option>
+                        <option value="ar-YE">ar-YE</option>
+                        <option value="cs-CZ">cs-CZ</option>
+                        <option value="da-DK">da-DK</option>
+                        <option value="de-AT">de-AT</option>
+                        <option value="de-CH">de-CH</option>
                         <option value="de-DE">de-DE</option>
+                        <option value="en-AU">en-AU</option>
+                        <option value="en-CA">en-CA</option>
+                        <option value="en-GB">en-GB</option>
+                        <option value="en-GH">en-GH</option>
+                        <option value="en-HK">en-HK</option>
+                        <option value="en-IE">en-IE</option>
+                        <option value="en-IN">en-IN</option>
+                        <option value="en-KE">en-KE</option>
+                        <option value="en-NG">en-NG</option>
+                        <option value="en-NZ">en-NZ</option>
+                        <option value="en-PH">en-PH</option>
+                        <option value="en-SG">en-SG</option>
+                        <option value="en-TZ">en-TZ</option>
+                        <option value="en-US">en-US</option>
+                        <option value="en-ZA">en-ZA</option>
+                        <option value="es-BO">es-BO</option>
+                        <option value="es-CL">es-CL</option>
+                        <option value="es-CO">es-CO</option>
+                        <option value="es-CR">es-CR</option>
+                        <option value="es-ES">es-ES</option>
+                        <option value="es-GQ">es-GQ</option>
+                        <option value="es-GT">es-GT</option>
+                        <option value="es-HN">es-HN</option>
+                        <option value="es-MX">es-MX</option>
+                        <option value="es-NI">es-NI</option>
+                        <option value="es-PA">es-PA</option>
+                        <option value="es-PE">es-PE</option>
+                        <option value="es-PR">es-PR</option>
+                        <option value="es-PY">es-PY</option>
+                        <option value="es-SV">es-SV</option>
+                        <option value="es-UY">es-UY</option>
+                        <option value="fi-FI">fi-FI</option>
+                        <option value="fr-FR">fr-FR</option>
+                        <option value="he-IL">he-IL</option>
+                        <option value="hi-IN">hi-IN</option>
+                        <option value="id-ID">id-ID</option>
                         <option value="it-IT">it-IT</option>
-                        {/* Add more options as needed */}
+                        <option value="ja-JP">ja-JP</option>
+                        <option value="ko-KR">ko-KR</option>
+                        <option value="pa-IN">pa-IN</option>
+                        <option value="pl-PL">pl-PL</option>
+                        <option value="pt-BR">pt-BR</option>
+                        <option value="pt-PT">pt-PT</option>
+                        <option value="ru-RU">ru-RU</option>
+                        <option value="sv-SE">sv-SE</option>
+                        <option value="th-TH">th-TH</option>
+                        <option value="tr-TR">tr-TR</option>
+                        <option value="ur-IN">ur-IN</option>
+                        <option value="zh-CN">zh-CN</option>
+                        <option value="zh-HK">zh-HK</option>
+                        <option value="zh-TW">zh-TW</option>
                     </select>
                 </div>
 
