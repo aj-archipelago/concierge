e5f272768fcc62ab17ed180b257f35190163b104
Jason McCartney <jmac@theroot.org>
2025-01-29 23:19:50 -0700

    Turn off jsdom tests in build environment

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,111 @@
+const projects = process.env.CI
+    ? [
+          {
+              displayName: "node",
+              testEnvironment: "node",
+              testMatch: ["<rootDir>/instrumentation.test.js"],
+              setupFiles: ["<rootDir>/jest.setup.js"],
+              transform: {
+                  "^.+\\.(js|jsx|mjs)$": [
+                      "babel-jest",
+                      {
+                          presets: [
+                              [
+                                  "@babel/preset-env",
+                                  { targets: { node: "current" } },
+                              ],
+                              "@babel/preset-react",
+                          ],
+                      },
+                  ],
+              },
+              moduleDirectories: ["node_modules", "__mocks__"],
+              moduleNameMapper: {
+                  "^@/(.*)$": "<rootDir>/@/$1",
+                  "^@components/(.*)$": "<rootDir>/@/components/$1",
+                  "^@lib/(.*)$": "<rootDir>/app/lib/$1",
+                  "^@utils/(.*)$": "<rootDir>/app/utils/$1",
+                  "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+                      "<rootDir>/__mocks__/fileMock.js",
+                  "\\.(css|less|scss|sass)$":
+                      "<rootDir>/__mocks__/styleMock.js",
+                  "^config/(.*)$": "<rootDir>/__mocks__/config/$1",
+                  "^./config/(.*)$": "<rootDir>/__mocks__/config/$1",
+              },
+          },
+      ]
+    : [
+          {
+              displayName: "components",
+              testEnvironment: "jsdom",
+              testMatch: ["<rootDir>/src/**/*.test.js"],
+              setupFiles: [
+                  "<rootDir>/jest.setup.js",
+                  "<rootDir>/__mocks__/setup.js",
+              ],
+              transform: {
+                  "^.+\\.(js|jsx|mjs)$": [
+                      "babel-jest",
+                      {
+                          presets: [
+                              [
+                                  "@babel/preset-env",
+                                  { targets: { node: "current" } },
+                              ],
+                              "@babel/preset-react",
+                          ],
+                      },
+                  ],
+              },
+              moduleDirectories: ["node_modules", "__mocks__"],
+              moduleNameMapper: {
+                  "^@/(.*)$": "<rootDir>/@/$1",
+                  "^@components/(.*)$": "<rootDir>/@/components/$1",
+                  "^@lib/(.*)$": "<rootDir>/app/lib/$1",
+                  "^@utils/(.*)$": "<rootDir>/app/utils/$1",
+                  "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+                      "<rootDir>/__mocks__/fileMock.js",
+                  "\\.(css|less|scss|sass)$":
+                      "<rootDir>/__mocks__/styleMock.js",
+                  "^config/(.*)$": "<rootDir>/__mocks__/config/$1",
+                  "^./config/(.*)$": "<rootDir>/__mocks__/config/$1",
+              },
+          },
+          {
+              displayName: "node",
+              testEnvironment: "node",
+              testMatch: ["<rootDir>/instrumentation.test.js"],
+              setupFiles: ["<rootDir>/jest.setup.js"],
+              transform: {
+                  "^.+\\.(js|jsx|mjs)$": [
+                      "babel-jest",
+                      {
+                          presets: [
+                              [
+                                  "@babel/preset-env",
+                                  { targets: { node: "current" } },
+                              ],
+                              "@babel/preset-react",
+                          ],
+                      },
+                  ],
+              },
+              moduleDirectories: ["node_modules", "__mocks__"],
+              moduleNameMapper: {
+                  "^@/(.*)$": "<rootDir>/@/$1",
+                  "^@components/(.*)$": "<rootDir>/@/components/$1",
+                  "^@lib/(.*)$": "<rootDir>/app/lib/$1",
+                  "^@utils/(.*)$": "<rootDir>/app/utils/$1",
+                  "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+                      "<rootDir>/__mocks__/fileMock.js",
+                  "\\.(css|less|scss|sass)$":
+                      "<rootDir>/__mocks__/styleMock.js",
+                  "^config/(.*)$": "<rootDir>/__mocks__/config/$1",
+                  "^./config/(.*)$": "<rootDir>/__mocks__/config/$1",
+              },
+          },
+      ];
+
 module.exports = {
     watchPathIgnorePatterns: ["\\.next"],
     modulePathIgnorePatterns: ["<rootDir>/.next", "<rootDir>/.next/standalone"],
@@ -8,73 +116,5 @@ module.exports = {
         enableSymlinks: false,
         throwOnModuleCollision: false,
     },
-    projects: [
-        {
-            displayName: "components",
-            testEnvironment: "jest-environment-jsdom",
-            testMatch: ["<rootDir>/src/**/*.test.js"],
-            setupFiles: [
-                "<rootDir>/jest.setup.js",
-                "<rootDir>/__mocks__/setup.js",
-            ],
-            transform: {
-                "^.+\\.(js|jsx|mjs)$": [
-                    "babel-jest",
-                    {
-                        presets: [
-                            [
-                                "@babel/preset-env",
-                                { targets: { node: "current" } },
-                            ],
-                            "@babel/preset-react",
-                        ],
-                    },
-                ],
-            },
-            moduleDirectories: ["node_modules", "__mocks__"],
-            moduleNameMapper: {
-                "^@/(.*)$": "<rootDir>/@/$1",
-                "^@components/(.*)$": "<rootDir>/@/components/$1",
-                "^@lib/(.*)$": "<rootDir>/app/lib/$1",
-                "^@utils/(.*)$": "<rootDir>/app/utils/$1",
-                "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-                    "<rootDir>/__mocks__/fileMock.js",
-                "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
-                "^config/(.*)$": "<rootDir>/__mocks__/config/$1",
-                "^./config/(.*)$": "<rootDir>/__mocks__/config/$1",
-            },
-        },
-        {
-            displayName: "node",
-            testEnvironment: "node",
-            testMatch: ["<rootDir>/instrumentation.test.js"],
-            setupFiles: ["<rootDir>/jest.setup.js"],
-            transform: {
-                "^.+\\.(js|jsx|mjs)$": [
-                    "babel-jest",
-                    {
-                        presets: [
-                            [
-                                "@babel/preset-env",
-                                { targets: { node: "current" } },
-                            ],
-                            "@babel/preset-react",
-                        ],
-                    },
-                ],
-            },
-            moduleDirectories: ["node_modules", "__mocks__"],
-            moduleNameMapper: {
-                "^@/(.*)$": "<rootDir>/@/$1",
-                "^@components/(.*)$": "<rootDir>/@/components/$1",
-                "^@lib/(.*)$": "<rootDir>/app/lib/$1",
-                "^@utils/(.*)$": "<rootDir>/app/utils/$1",
-                "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-                    "<rootDir>/__mocks__/fileMock.js",
-                "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
-                "^config/(.*)$": "<rootDir>/__mocks__/config/$1",
-                "^./config/(.*)$": "<rootDir>/__mocks__/config/$1",
-            },
-        },
-    ],
+    projects,
 };
