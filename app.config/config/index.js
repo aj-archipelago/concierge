cd8f533c3918a3889a81fbaf12b9a346d088d3dc
Hammad Siddiqui <hammadzs@gmail.com>
2025-02-07 14:01:58 -0500

    Revert app.config changes

--- app.config/config/index.js
+++ app.config/config/index.js
@@ -1,76 +1,22 @@
-import { getTosContent } from "./global/tos";
-import { getSidebarLogo } from "./global/sidebar";
-import { FaFileImport } from "react-icons/fa";
-import { ImportSuggestions } from "./write/actions/ImportSuggestions";
-import { getTags, getTaxonomySets, getTopics } from "./data/taxonomySets";
-import { basePath } from "../../src/utils/constants";
-import { fetchUrlSource } from "./transcribe/TranscribeUrlConstants";
-
-const cortexURLs = {
-    dev: "https://cortex.aljazeera.com/dev/graphql?subscription-key=<your key>",
-    prod: "https://cortex.aljazeera.com/graphql?subscription-key=<your key>",
-};
+// This is the master config for the concierge app.
+// To override a particular section, define it here.
+// To see the complete config schema, please refer to
+// /config/default/config/index.js
 
 export default {
-    global: {
-        siteTitle: "Labeeb",
-        getLogo: (language) =>
-            (basePath || "") +
-            `/app/assets/labeeb-logo-${language === "ar" ? "ar" : "en"}.png`,
-        getTosContent,
-        getSidebarLogo,
-        getPublicGraphQLEndpoint: (graphQLEndpoint = "") => {
-            if (graphQLEndpoint.includes("cortex-internal-dev")) {
-                return cortexURLs["dev"];
-            } else if (graphQLEndpoint.includes("cortex-internal")) {
-                return cortexURLs["prod"];
-            }
-
-            return graphQLEndpoint;
-        },
-    },
-    data: {
-        getTaxonomySets,
-        getTopics,
-        getTags,
-    },
-    write: {
-        actions: {
-            import: {
-                Icon: FaFileImport,
-                type: "always-available",
-                title: "Import from UCMS",
-                dialogClassName: "modal-narrow",
-                commitLabel: "Import",
-                SuggestionsComponent: ImportSuggestions,
-                postApply: "clear-headline",
-            },
-        },
-    },
-    chat: {
-        botName: "Labeeb",
-        dataSources: [
-            {
-                key: "wires",
-                name: "Wires",
-                description: "use wires",
-            },
-            {
-                key: "aja",
-                name: "Al Jazeera Arabic articles",
-                description: "use aj arabic articles",
-            },
-            {
-                key: "aje",
-                name: "Al Jazeera English articles",
-                description: "use aj english articles",
-            },
-        ],
-    },
-    transcribe: {
-        fetchUrlSource,
-    },
-    auth: {
-        provider: "entra",
-    },
+    // global: {
+    //   ...
+    // },
+    // data: {
+    //   ...
+    // },
+    // write: {
+    //   ...
+    // },
+    // chat: {
+    //   ...
+    // },
+    // code: {
+    //   ...
+    // }
 };
