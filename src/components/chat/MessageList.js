db43b0033d98c820b4401e1d63bfd5fcde29d34c
Jason McCartney <jmac@theroot.org>
2025-02-13 10:34:43 -0700

    Prettier fixes

--- src/components/chat/MessageList.js
+++ src/components/chat/MessageList.js
@@ -287,8 +287,18 @@ function MessageList({ messages, bot, loading, chatId, isStreaming }) {
                             >
                                 {message.payload}
                                 {message.isStreaming && (
-                                    <div style={{ height: "1.2em", marginTop: "0.2em" }}>
-                                        <Loader style={{ width: "0.6em", height: "0.6em" }} />
+                                    <div
+                                        style={{
+                                            height: "1.2em",
+                                            marginTop: "0.2em",
+                                        }}
+                                    >
+                                        <Loader
+                                            style={{
+                                                width: "0.6em",
+                                                height: "0.6em",
+                                            }}
+                                        />
                                     </div>
                                 )}
                             </div>
