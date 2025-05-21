358e4c327cdafb53403a1c7c7f2a9aa111685e20
Jason McCartney <33706922+jmacAJ@users.noreply.github.com>
2025-05-21 16:12:20 -0700

    Merge pull request #385 from ALJAZEERAPLUS/hammads_job_logging
    Send job execution logs to bullmq + view in admin

--- jobs/logger.js
+++ jobs/logger.js
@@ -1,10 +1,16 @@
 const dayjs = require("dayjs");
+const { Job, Queue } = require("bullmq");
+const { getRedisConnection } = require("../app/api/utils/redis.mjs");
 const logTimestamp = () => dayjs().format("YYYY-MM-DD HH:mm:ss:SSS");
 
 class Logger {
-    constructor({ id, name } = {}) {
+    constructor({ id, name, queueName } = {}) {
         this.id = id;
         this.name = name;
+        if (queueName) {
+            const connection = getRedisConnection();
+            this.queue = new Queue(queueName, { connection });
+        }
     }
 
     log(message, ...debug) {
@@ -25,6 +31,14 @@ class Logger {
 
         const elements = [`${logTimestamp()}${prefix}`, message];
         console.log(elements.filter(Boolean).join(": "));
+
+        if (this.queue) {
+            Job.addJobLog(
+                this.queue,
+                this.id,
+                `${logTimestamp()}${prefix}: ${message} ${debugInfo ? `[${debugInfo}]` : ""}`,
+            );
+        }
     }
 }
 
--- jobs/worker.js
+++ jobs/worker.js
@@ -76,7 +76,7 @@ const worker = new Worker(
         try {
             await connectToDatabase();
 
-            const logger = new Logger(job);
+            const logger = new Logger(job, digestBuild);
 
             if (job.name === PERIODIC_BUILD_JOB) {
                 logger.log("building digests for all users");
@@ -96,12 +96,12 @@ const worker = new Worker(
 );
 
 worker.on("completed", (job) => {
-    const logger = new Logger(job);
+    const logger = new Logger(job, digestBuild);
     logger.log("job completed");
 });
 
 worker.on("failed", (job, error) => {
-    const logger = new Logger(job);
+    const logger = new Logger(job, digestBuild);
     logger.log("job failed with error: " + error.message);
 });
 
