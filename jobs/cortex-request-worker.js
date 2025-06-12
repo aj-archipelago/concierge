d6f34506b4f08460593633de9458a297f674d004
Hammad Siddiqui <hammadzs@gmail.com>
2025-06-12 15:03:36 -0400

    Bump concurrency to 20

--- jobs/cortex-request-worker.js
+++ jobs/cortex-request-worker.js
@@ -22,7 +22,7 @@ import("../app/api/utils/redis.mjs").then((module) => {
         {
             connection,
             autorun: false,
-            concurrency: 5,
+            concurrency: 20,
         },
     );
 
